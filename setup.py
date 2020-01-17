2c7ae22f1b5b3b6c39b7952024fa901c1a786092
xela <xelaxela13@gmail.com>
2020-01-17 23:30:16 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -3,29 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name="django-include-bootstrap",
     version="1.0",
-    packages=find_packages(),
-    # scripts=["say_hello.py"],
-
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     install_requires=["requests", "subresource-integrity"],
-
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         "": ["*.txt", "*.rst", "*.msg"],
     },
-
     # metadata to display on PyPI
     author="Alex Stepanenko",
     author_email="xelaxela13@gmail.com",
     description="Include twitter bootstrap to Django templates",
     keywords="django, bootstrap",
-    url="https://github.com/xelaxela13/django_include_bootstrap",  # project home page, if any
-    # project_urls={
-    #     "Bug Tracker": "https://bugs.example.com/HelloWorld/",
-    #     "Documentation": "https://docs.example.com/HelloWorld/",
-    #     "Source Code": "https://code.example.com/HelloWorld/",
-    # },
+    url="https://github.com/xelaxela13/django_include_bootstrap",
     classifiers=[
         "License :: OSI Approved :: Python Software Foundation License",
         "Environment :: Web Environment",
@@ -40,5 +30,4 @@ setup(
         "Topic :: Software Development :: Libraries"
     ],
     include_package_data=True
-
 )
